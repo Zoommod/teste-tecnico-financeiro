b0a30b9a9491865b67e81d64bccc6595ee914067
Zoommod <vitor.sacramento09@gmail.com>
2026-03-12 13:05:28 -0300

    chore(infra): adiciona extensões de configuração para injeção de dependência

---
+++ backend/Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.Design;
+using Application.Interfaces;
+using Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IPessoaService, PessoaService>();
+        services.AddScoped<ICategoriaService, CategoriaService>();
+        services.AddScoped<ITransacaoService, TransacaoService>();
+        services.AddScoped<IRelatorioService, RelatorioService>();
+        return services;
+    }
+}
---
+++ backend/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+using System;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options => options.UseSqlite(configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+        services.AddScoped<IPessoaRepository, PessoaRepository>();
+        services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+        services.AddScoped<ITransacaoRepository, TransacaoRepository>();
+
+        return services;
+    }
+}
